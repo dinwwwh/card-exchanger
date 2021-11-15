4a5f645dd300116b101b74d5812c6f82f87a7db6
dinhdjj <dinhdjj@gmail.com>
2021-11-15 20:13:58 +0700

    build: integrate `tailwindcss`

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="vi">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Đổi thẻ cào nhanh chóng </title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,5 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .vue(3)
-    .postCss("resources/css/app.css", "public/css", [
-        //
-    ])
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
     .version();
