6844a119911580b56f4f6621ec47c0f1fed1cfa6
dinhdjj <dinhdjj@gmail.com>
2021-11-18 20:23:15 +0700

    build(webpack): declare alias `~`

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .alias({
+        '~': 'resources/js/',
+    })
     .extract()
     .vue(3)
     .postCss('resources/css/app.css', 'public/css', [require('tailwindcss')])
